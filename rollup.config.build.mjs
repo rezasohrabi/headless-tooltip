b867ea31774a6d9bb578e86ec086c8354e23c957
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-06-04 20:42:57 +0330

    refactor: read package.json using fs instead of import assertion

--- rollup.config.build.mjs
+++ rollup.config.build.mjs
@@ -7,8 +7,9 @@ import postcss from 'rollup-plugin-postcss';
 import json from '@rollup/plugin-json';
 import dts from 'rollup-plugin-dts';
 import analyze from 'rollup-plugin-analyzer';
+import fs from 'fs';
 
-import pkg from './package.json' assert { type: 'json' };
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
 
 export default [
   {
