8560c1e5cfd72d3959939729ad3d44381b0baf31
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-08-30 00:36:38 +0330

    refactor: update exports in index files to streamline component access

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -237,3 +237,5 @@ export function Tooltip({
     </>
   );
 }
+
+export default Tooltip;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+export { default } from './components/Tooltip';
 export { Tooltip } from './components';
 export type { TooltipProps } from './types';
 export type { Placement } from './types';
