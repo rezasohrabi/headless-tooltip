46919fd961193db43e95355daaac168ae629f3bf
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-09-18 23:28:53 +0330

    feat: add JSXContentSection component with interactive tooltips and styles, and integrate it into the homepage

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,12 +3,14 @@ import 'animate.css';
 import './animations.css';
 import DemoSection from '../components/landing/demo-section/DemoSection';
 import LibrariesSection from '../components/landing/libraries-section/LibrariesSection';
+import JSXContentSection from '../components/landing/jsx-content-section/JSXContentSection';
 
 export default function Home() {
   return (
     <>
       <DemoSection />
       <LibrariesSection />
+      <JSXContentSection />
     </>
   );
 }
